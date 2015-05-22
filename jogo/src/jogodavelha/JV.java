38aadecfba14cd7cf2af87951786f7117312e6d0
joseano rodrigues <joseano.23jo@gmail.com>
2015-05-22 19:50:51 -0300

    commit implementando metodo jogadas

--- jogo/src/jogodavelha/JV.java
+++ jogo/src/jogodavelha/JV.java
@@ -36,4 +36,27 @@ public class JV {
        return false;
         
     }
+  public void Jogada(String p, String j){
+           
+       
+        if(p.equals("1"))
+            m[0][0] = j;
+        else if(p.equals("2"))
+            m[0][1] = j;
+        else if(p.equals("3"))
+            m[0][2] = j;
+        else if(p.equals("4"))
+            m[1][0] = j;
+        else if(p.equals("5"))
+            m[1][1] = j;
+        else if(p.equals("6"))
+            m[1][2] = j;
+        else if(p.equals("7"))
+            m[2][0] = j;
+        else if(p.equals("8"))
+            m[2][1] = j;
+        else if(p.equals("9"))
+            m[2][2] = j;
+    }
+
 }
